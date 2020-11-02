f52fe3af4a5ae787455b62c621ec11036e843b74
Walter <walter.e.pacheco.chandia@gmail.com>
2020-11-02 01:55:27 -0300

    se crea semaforo en react

---
+++ src/componet/TrafficLigth.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+ function TrafficLight() {
+	const [currentLight, setLight] = useState("yellow");
+	return (
+        <div id="trafficTop">
+		    <div className="traffic-light">
+		    	<div className="protector"></div>
+                <div className="protector"></div>
+                <div className="protector"></div>
+		    	<div className="trafficContainer">
+		    		{currentLight === "red" ? (
+		    			<div className="red light selected" />
+		    		) : (
+		    			<div
+		    				className="red light"
+		    				onClick={() => setLight("red")}
+		    			/>
+		    		)}
+		    		{currentLight === "yellow" ? (
+		    			<div className="yellow light selected" />
+		    		) : (
+		    			<div
+		    				className="yellow light"
+		    				onClick={() => setLight("yellow")}
+		    			/>
+		    		)}
+		    		{currentLight === "green" ? (
+		    			<div className="green light selected" />
+		    		) : (
+		    			<div
+		    				className="green light"
+		    				onClick={() => setLight("green")}
+		    			/>
+		    		)}
+		    	</div>
+		    </div>
+        </div>    
+
+	);
+}
+export default TrafficLight;
